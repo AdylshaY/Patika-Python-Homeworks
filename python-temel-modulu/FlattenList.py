6f07d7e2d51c10a10fb25104ba416407ab03ee77
Adylsha Yumayev <68568793+AdylshaY@users.noreply.github.com>
2022-01-14 11:12:13 +0300

    Update FlattenList.py

--- python-temel-modulu/FlattenList.py
+++ python-temel-modulu/FlattenList.py
@@ -1,22 +1,17 @@
 array = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
 
+def FlattenList(array):
+    for i in range(0,len(array)):
+        if IsList(array[i]):
+            FlattenList(array[i])
+        else:
+            l.append(array[i])
+    return l
+
 def IsList(array):
     if type(array) == list:
         return True
     else:
         return False
     
-def FlattenList(array):
-    global l
-    l=[]
-    length = len(array)
-    for i in range(0,length):
-        if IsList(array[i]):
-            f(array[i])
-        else:
-            l.append(array[i])
-    return l
-  
-l=[]
-
-print(FlattenList(array))
+l = []
